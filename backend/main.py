1e5680890be7e5cd47b0f0c21f88b92da4adb6f8
Haga Kengo <chon01204@gmail.com>
2026-04-20 19:51:28 +0900

    fix: add EC2 IP to CORS settings

--- backend/main.py
+++ backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI(title="Task Manager API")
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["http://localhost:3000"],
+    allow_origins=["http://localhost:3000""http://18.181.247.4:3000"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
